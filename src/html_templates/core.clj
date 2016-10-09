be0b300327126f65b17804ea300c657cc28e6420
Dominik Piatek <do.piatek@gmail.com>
2016-10-09 20:01:18 +0100

    Init

---
+++ src/html_templates/core.clj
@@ -0,0 +1,6 @@
+(ns html-templates.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/html_templates/core_test.clj
@@ -0,0 +1,7 @@
+(ns html-templates.core-test
+  (:require [clojure.test :refer :all]
+            [html-templates.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
