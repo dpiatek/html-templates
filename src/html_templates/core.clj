7e728c0dcaf4ce0eb4fa96d5ae5f6b1a18c65f69
Dominik Piatek <do.piatek@gmail.com>
2016-10-09 21:32:59 +0100

    Templates engine

--- src/html_templates/core.clj
+++ src/html_templates/core.clj
@@ -1,6 +1,25 @@
 (ns html-templates.core)
 
-(defn foo
-  "I don't do a whole lot."
+(defn attrs
+  [attr-map]
+  {:pre [(or (map? attr-map)
+             (nil? attr-map))]}
+  (->> attr-map
+    (mapcat (fn [[k v]] [\space (name k) "=\"" v "\""]))
+    (apply str)))
+
+(defn html
   [x]
-  (println x "Hello, World!"))
+  {:pre [(if (sequential? x)
+           (some #(-> x first %) [keyword? symbol? string?])
+           (not (map? x)))]
+   :post [(string? %)]}
+  (if-not (sequential? x)
+    (str x)
+    (let [[tag & body] x
+          [attr-map body] (if (map? (first body))
+                            [(first body) (rest body)]
+                            [nil body])]
+      (str "<" (name tag) (attrs attr-map) ">"
+           (apply str (map html body))
+           "</" (name tag) ">"))))
--- test/html_templates/core_test.clj
+++ test/html_templates/core_test.clj
@@ -1,7 +1,37 @@
 (ns html-templates.core-test
   (:require [clojure.test :refer :all]
-            [html-templates.core :refer :all]))
+            [html-templates.core :refer :all]
+            [clojure.string]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defmacro are* [f & body]
+  `(are [x# y#] (~'= (~f x#) y#)
+     ~@body))
+
+; (declare html attrs)
+
+(deftest test-html
+  (are* html
+    [:html]
+    "<html></html>"
+
+    [:a [:b]]
+    "<a><b></b></a>"
+
+    [:a {:href "/"} "Home"]
+    "<a href=\"/\">Home</a>"
+
+    [:div "foo" [:span "bar"] "baz"]
+    "<div>foo<span>bar</span>baz</div>"
+
+    (list* :ul (for [author [\1 \2 \3]] [:li author]))
+    "<ul><li>1</li><li>2</li><li>3</li></ul>"))
+
+(deftest test-attrs
+  (are* (comp clojure.string/trim attrs)
+    nil ""
+
+    {:foo "bar"}
+    "foo=\"bar\""
+
+    (sorted-map :a "b" :c "d")
+    "a=\"b\" c=\"d\""))
